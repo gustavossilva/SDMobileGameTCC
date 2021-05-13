e3a0339d31b2d094d306c7747d907de3d07a1f4e
Gustavo Silva <xgustavossilva@gmail.com>
2021-05-13 01:26:19 -0300

    Add game mode 2 to circuit

--- Assets/LogicOperator.cs
+++ Assets/LogicOperator.cs
@@ -4,15 +4,28 @@ public class LogicOperator : MonoBehaviour
 {
     public GameObject and;
     public GameObject or;
+
+    public int position = 0;
     // Start is called before the first frame update
     void Start()
     {
-        ShipGameManager.Instance.SetGameOperation("and");
+        bool startWithOr = Random.Range(0.0f, 1.0f) > 0.5;
+        if (startWithOr) {
+            and.SetActive(false);
+            or.SetActive(true);
+            ShipGameManager.Instance.SetGameOperation("or", position);
+        }
+        else {
+            ShipGameManager.Instance.SetGameOperation("and", position);
+        }
     }
 
     public void SwitchOperator() {
+        if (ShipGameManager.Instance.lockLogicOperators) {
+            return;
+        }
         string operation = this.and.activeSelf ? "or" : "and";
-        ShipGameManager.Instance.SetGameOperation(operation);
+        ShipGameManager.Instance.SetGameOperation(operation, position);
         this.and.SetActive(!this.and.activeSelf);
         this.or.SetActive(!this.or.activeSelf);
         // tells gameManager the change
--- Assets/ShipGameManager.cs
+++ Assets/ShipGameManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class ShipGameManager : Singleton<ShipGameManager>
 {
+    public bool lockLogicOperators = true;
     public string gameType = "easy";
     public GameObject finalTrail;
-
-    public List<bool> game1 = new List<bool>(new bool[2]);
+    public GameObject finalTrailGame2;
+    public GameObject midTrail1;
+    public GameObject midTrail2;
+    public GameObject midTrail1Active;
+    public GameObject midTrail2Active;
+    public List<bool> gameSwitches = new List<bool>(new bool[4]);
+    public List<string> gameOperations = new List<string>(new string[4]);
     public string game1Operation = "and";
     // Start is called before the first frame update
     protected override void Awake()
@@ -17,18 +23,80 @@ public class ShipGameManager : Singleton<ShipGameManager>
     }
 
     public void SetGameArray(bool value, int position) {
-        if (gameType == "easy") { 
-            game1[position] = value;
+        gameSwitches[position] = value;
+        if (gameType == "hard") {
+            if (position == 0 || position == 1) {
+                bool active = gameOperations[0] == "and" ? gameSwitches[0] && gameSwitches[1] : gameSwitches[0] || gameSwitches[1];
+                if (active) {
+                    midTrail1.SetActive(false);
+                    midTrail1Active.SetActive(true);
+                } else {
+                    midTrail1.SetActive(true);
+                    midTrail1Active.SetActive(false);
+                }
+            }
+            if (position == 2 || position == 3) {
+                bool active = gameOperations[1] == "and" ? gameSwitches[2] && gameSwitches[3] : gameSwitches[2] || gameSwitches[3];
+                if (active) {
+                    midTrail2.SetActive(false);
+                    midTrail2Active.SetActive(true);
+                } else {
+                    midTrail2.SetActive(true);
+                    midTrail2Active.SetActive(false);
+                }
+            }
+            /*if (position == 3) {
+                bool active = gameOperations[2] == "and" ? midTrail1Active.activeSelf && midTrail2Active.activeSelf : midTrail1Active.activeSelf || midTrail2Active.activeSelf;
+                if (active) {
+                    midTrail2.SetActive(false);
+                    midTrail2Active.SetActive(true);
+                } else {
+                    midTrail2.SetActive(true);
+                    midTrail2Active.SetActive(false);
+                }
+            }*/
         }
     }
 
-    public void SetGameOperation(string operation) {
-        game1Operation = operation;
+    public void SetGameOperation(string operation, int position) {
+        gameOperations[position] = operation;
+        if (gameType == "hard") {
+            if (position == 0) {
+                bool active = gameOperations[0] == "and" ? gameSwitches[0] && gameSwitches[1] : gameSwitches[0] || gameSwitches[1];
+                if (active) {
+                    midTrail1.SetActive(false);
+                    midTrail1Active.SetActive(true);
+                } else {
+                    midTrail1.SetActive(true);
+                    midTrail1Active.SetActive(false);
+                }
+            }
+            if (position == 1) {
+                bool active = gameOperations[1] == "and" ? gameSwitches[2] && gameSwitches[3] : gameSwitches[2] || gameSwitches[3];
+                if (active) {
+                    midTrail2.SetActive(false);
+                    midTrail2Active.SetActive(true);
+                } else {
+                    midTrail2.SetActive(true);
+                    midTrail2Active.SetActive(false);
+                }
+            }
+            /*if (position == 3) {
+                bool active = gameOperations[2] == "and" ? midTrail1Active.activeSelf && midTrail2Active.activeSelf : midTrail1Active.activeSelf || midTrail2Active.activeSelf;
+                if (active) {
+                    midTrail2.SetActive(false);
+                    midTrail2Active.SetActive(true);
+                } else {
+                    midTrail2.SetActive(true);
+                    midTrail2Active.SetActive(false);
+                }
+            }*/
+        }
     }
 
     public void Send() {
         if (gameType == "easy") {
-            bool winner = game1Operation == "and" ? game1[0] && game1[1] : game1[0] || game1[1];
+            bool winner = gameOperations[0] == "and" ? gameSwitches[0] && gameSwitches[1] : gameSwitches[0] || gameSwitches[1];
             if (!winner) {
                 // losthp
                 Debug.Log("Wrong");
@@ -36,6 +104,16 @@ public class ShipGameManager : Singleton<ShipGameManager>
                 Debug.Log("Correct");
                 // add point, generate new game
             }
+        }
+        if (gameType == "hard") {
+            bool winner = gameOperations[2] == "and" ? midTrail1Active.activeSelf && midTrail2Active.activeSelf : midTrail1Active.activeSelf || midTrail2Active.activeSelf;
+            if (!winner) {
+                // losthp
+                Debug.Log("Wrong");
+            } else {
+                Debug.Log("Correct");
+                // add point, generate new game
+            }      
         }
     }
 }
